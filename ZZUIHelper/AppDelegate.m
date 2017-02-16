13074966149efb044279922b4300000a16f5d7ee
李伯坤 <libokun@58ganji.com>
2017-02-16 20:44:54 +0800

    Initial Commit

---
+++ ZZUIHelper/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  ZZUIHelper
+//
+//  Created by 李伯坤 on 2017/2/16.
+//  Copyright © 2017年 李伯坤. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ ZZUIHelper/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ZZUIHelper
+//
+//  Created by 李伯坤 on 2017/2/16.
+//  Copyright © 2017年 李伯坤. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
