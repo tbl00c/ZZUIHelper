062907de85735f7d8de520e4cc07b8327c2dc861
李伯坤 <libokun@58ganji.com>
2017-02-20 18:06:42 +0800

    控件顺序微调

--- ZZUIHelper/Business/ZZControlHelper.m
+++ ZZUIHelper/Business/ZZControlHelper.m
@@ -24,8 +24,8 @@
 {
 //    return @[@{@"ClassName" : @"UIView"}];
     return @[
-             @"UILabel",
              @"UIImageView",
+             @"UILabel",
              @"UIButton",
              @"UIView",
              @"UITableView",
